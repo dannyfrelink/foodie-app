eeda1fe7a4ee2d689b28782b834c80fbdfb39dfb
dannyfrelink <danny-frelink@hotmail.com>
2022-02-14 14:18:54 +0100

    test barcode detector

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 let barcode = 3168930158158
 
-fetch(`https://world.openfoodfacts.org/api/v0/product/` + barcode + `.json`)
-    .then(res => res.json())
-    .then(data => console.log(data))
+const fetchData = () => {
+    fetch(`https://world.openfoodfacts.org/api/v0/product/` + barcode + `.json`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+
+// create new detector
+var barcodeDetector = new BarcodeDetector({ formats: ['code_39', 'codabar', 'ean_13'] });
+
+// console.log(barcodeDetector)
+
+// check compatibility
+if (barcodeDetector) {
+    console.log('Barcode Detector supported!');
+} else {
+    console.log('Barcode Detector is not supported by this browser.');
+}
+
+// check supported types
+BarcodeDetector.getSupportedFormats()
+    .then(supportedFormats => {
+        supportedFormats.forEach(format => console.log(format));
+    });
+
+var img = document.createElement('img');
+img.src = 'barcode.png';
+
+barcodeDetector.detect(img)
+    .then(barcodes => {
+        barcodes.forEach(barcode => console.log(barcode.rawData));
+    })
+    .catch(err => {
+        console.log(err);
+    })
