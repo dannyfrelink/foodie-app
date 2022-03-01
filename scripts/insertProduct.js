99b2933b724e6aed13d4023a194860faa2f355ab
dannyfrelink <danny-frelink@hotmail.com>
2022-03-01 12:35:36 +0100

    empty state for product info

--- scripts/insertProduct.js
+++ scripts/insertProduct.js
@@ -5,32 +5,43 @@ import { barcodeInput } from './searchBarcode.js';
 export default function insertProduct(data) {
     showProductSection();
     barcodeInput.value = '';
-
     const productInfo = data.product;
-    const ingredients = productInfo.ingredients_text_en;
-    const ingredientsArray = ingredients.split(',');
-    const allergies = productInfo.allergens_imported;
-    const allergiesArray = allergies.split(',');
 
     nameProduct.innerHTML = productInfo.product_name;
     imgProduct.src = productInfo.image_url;
     quantityProduct.innerHTML = `Quantity: ${productInfo.quantity}`;
 
-    ulIngredients.innerHTML = '';
-    ingredientsArray.forEach(ingredient => {
-        let listItemIngredient = document.createElement('li');
-        if (ingredient !== '') {
-            listItemIngredient.innerHTML = ingredient;
-            ulIngredients.appendChild(listItemIngredient);
-        }
-    })
+    if (productInfo.ingredients_text_en) {
+        const ingredients = productInfo.ingredients_text_en;
+        const ingredientsArray = ingredients.split(',');
 
-    ulAllergies.innerHTML = '';
-    allergiesArray.forEach(allergy => {
-        let listItemAllergy = document.createElement('li');
-        if (allergy !== '') {
-            listItemAllergy.innerHTML = allergy;
-            ulAllergies.appendChild(listItemAllergy);
-        }
-    })
+        ulIngredients.innerHTML = '';
+        ingredientsArray.forEach(ingredient => {
+            let listItemIngredient = document.createElement('li');
+            if (ingredient !== '') {
+                listItemIngredient.innerHTML = ingredient;
+                ulIngredients.appendChild(listItemIngredient);
+            }
+        })
+    }
+    else {
+        ulIngredients.innerHTML = 'none'
+    }
+
+    if (productInfo.allergens_imported) {
+        const allergies = productInfo.allergens_imported;
+        const allergiesArray = allergies.split(',');
+
+        ulAllergies.innerHTML = '';
+        allergiesArray.forEach(allergy => {
+            let listItemAllergy = document.createElement('li');
+            if (allergy !== '') {
+                listItemAllergy.innerHTML = allergy;
+                ulAllergies.appendChild(listItemAllergy);
+            }
+        })
+    }
+    else {
+        ulAllergies.innerHTML = 'none'
+    }
 }
