748c85539b6559b5c800f604f3c88f60d8d42564
dannyfrelink <danny-frelink@hotmail.com>
2022-03-09 11:18:01 +0100

    remove access code

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -5,9 +5,8 @@ export async function fetchWithBarcode(barcodeValue) {
     let barcode = barcodeValue;
     await fetch(`https://world.openfoodfacts.org/api/v0/product/${barcode}.json`)
         .then((response) => response.json())
-        .then(async (data) => {
+        .then((data) => {
             stopLoader();
-            // checkBarcode.check(data);
             checkBarcode.init(data);
         })
         .catch((err) => {
